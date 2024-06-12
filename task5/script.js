566ded5e3dd3a0763b1544a01e05a2454fcc9919
Dmitrousov <artem.dmitrousov@yandex.ru>
2024-06-12 19:56:13 +0500

    update

--- task5/script.js
+++ task5/script.js
@@ -1,14 +1,14 @@
-const inputField = document.querySelector('inputField');
-const duplicateField = document.querySelector('duplicateField');
-const submitButton = document.querySelector('submitButton');
+const inputField = document.querySelector('#inputField');
+const doubleField = document.querySelector('#duplicateField');
+const submitButton = document.querySelector('#submitButton');
 
-inputField.addEventListener('input', functiion(event) = {
-    duplicateField.textContent = inputField.value
-});
+inputField.addEventListener('input', function () {
+    doubleField.textContent = inputField.value
+})
 
 submitButton.addEventListener('click', function(event) {
     event.preventDefault();
     const inputValue = inputField.value;
-    console.log('inputField.value');
+    console.log(inputField.value);
     inputField.value = '';
 });
